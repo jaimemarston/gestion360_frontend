babf673a4406bbb4a74dbe15bb6ac7211f662195
xiover <xioveraldana123@gmail.com>
2024-05-13 12:06:20 -0400

    show the sidebar with icons and adjust the file upload component and the size of each file

--- src/Components/Sistema/Menu/FileManager/index.jsx
+++ src/Components/Sistema/Menu/FileManager/index.jsx
@@ -7,6 +7,7 @@ import { FileUploader } from "react-drag-drop-files";
 import { RegisterFolder } from "./register-folder";
 import { RegisterGroup } from "./register-group";
 import "./style-file-manager.scss";
+import FileExplorer from "./test";
 
 export default function FileManager() {
   const [selectedItemId, setSelectedItemId] = useState(null);
@@ -21,12 +22,11 @@ export default function FileManager() {
     );
     if (pdfFiles.length > 0) {
       // Concatenar los nuevos archivos PDF al array existente
-      setFiles((prevFiles) => [...prevFiles,...pdfFiles]);
+      setFiles((prevFiles) => [...prevFiles, ...pdfFiles]);
     } else {
       alert("Solo se permiten archivos PDF.");
     }
   };
-
 
   const handleItemClick = (itemId) => {
     setSelectedItemId(itemId);
@@ -39,161 +39,93 @@ export default function FileManager() {
 
   const fileTypes = ["pdf"];
 
-  const Arr = [
+  const ITEMS = [
     {
-      id: 1,
-      father: "item-father-1",
-      // Nivel 2
-      secondLevel: [
+      id: "1",
+      label: "item-father-1",
+      children: [
         {
-          id: 2,
-          title: "second-level-1",
-          // Nivel 3
-          thirdLevel: [
-            { id: 3, title: "Ultimo nivel1-item1" },
-            { id: 4, title: "Ultimo nivel1-item2" },
-            { id: 5, title: "Ultimo nivel1-item3" },
+          id: "1.1",
+          label: "second-level-1",
+          children: [
+            { id: "1.1.1", label: "Ultimo nivel1-item1" },
+            { id: "1.1.2", label: "Ultimo nivel1-item2" },
+            { id: "1.1.3", label: "Ultimo nivel1-item3" },
           ],
         },
       ],
     },
     {
-      id: 7,
-      father: "item-father-2",
-      // Nivel 2 opcional
-      secondLevel: [
+      id: "2",
+      label: "Bookmarked",
+      fileType: "pinned",
+      children: [
         {
-          id: 8,
-          title: "second-level-2",
-          // Nivel 3 opcional
-          thirdLevel: [
-            { id: 9, title: "Ultimo nivel2-item1" },
-            { id: 10, title: "Ultimo nivel2-item2" },
-            { id: 11, title: "Ultimo nivel2-item3" },
-          ],
+          id: "2.1",
+          label: "Learning materials",
         },
+        { id: "2.2", label: "News" },
+        { id: "2.3", label: "Forums" },
+        { id: "2.4", label: "Travel documents" },
       ],
     },
-    {
-      id: 13,
-      father: "item-father-3",
-      // Solo un nivel
-    },
-    {
-      id: 14,
-      father: "item-father-4",
-      // Nivel 2
-      secondLevel: [
-        {
-          id: 15,
-          title: "second-level-3",
-          // Nivel 3
-          thirdLevel: [
-            { id: 16, title: "Ultimo nivel3-item1" },
-            { id: 17, title: "Ultimo nivel3-item2" },
-            { id: 18, title: "Ultimo nivel3-item3" },
-          ],
-        },
-      ],
-    },
-    {
-      id: 20,
-      father: "item-father-5",
-      // Solo un nivel
-    },
-    {
-      id: 21,
-      father: "item-father-6",
-      secondLevel: [
-        {
-          id: 22,
-          title: "second-level-6",
-          thirdLevel: [
-            { id: 25, title: "Ultimo nivel3-item1" },
-            { id: 26, title: "Ultimo nivel3-item2" },
-            { id: 27, title: "Ultimo nivel3-item3" },
-          ],
-        },
-        { id: 23, title: "second-level-7" },
-        { id: 24, title: "second-level-8" },
-      ],
-    },
+    { id: "3", label: "History" },
+    { id: "4", label: "Trash" },
   ];
 
   return (
     <div className="container">
       <div className="row">
-      <div className="col-10">
-        <RegisterGroup />
-      </div>
-      <div className="col-2">
-        {showModal ? (
-          <RegisterFolder />
-        ) : (<></>)}
-      </div>
+        <div className="col-10">
+          <RegisterGroup />
+        </div>
+        <div className="col-2">{showModal ? <RegisterFolder /> : <></>}</div>
         <div className="col-6">
-          <Box sx={{ height: 220, maxWidth: 400 }}>
-            {Arr.map((item, index) => (
-              <SimpleTreeView key={index}>
-                <TreeItem
-                  itemId={item.id}
-                  label={item.father}
-                  onClick={() => {handleItemClick(item.id), setShowModal(true)}}
-                >
-                  {item.secondLevel &&
-                    item.secondLevel.map((secondLevelItem) => (
-                      <TreeItem
-                        key={secondLevelItem.id}
-                        itemId={secondLevelItem.id}
-                        label={secondLevelItem.title}
-                        onClick={() => {handleItemClick(secondLevelItem.id), setShowModal(true)}} // Manejar clic en el segundo nivel
-                      >
-                        {secondLevelItem.thirdLevel &&
-                          secondLevelItem.thirdLevel.map((thirdLevelItem) => (
-                            <TreeItem
-                              key={thirdLevelItem.id}
-                              itemId={thirdLevelItem.id}
-                              label={thirdLevelItem.title}
-                              onClick={() => {handleItemClick(thirdLevelItem.id), setShowModal(true)}} // Manejar clic en el tercer nivel
-                            />
-                          ))}
-                      </TreeItem>
-                    ))}
-                </TreeItem>
-              </SimpleTreeView>
-            ))}
-          </Box>
+          <FileExplorer date={ITEMS} />
         </div>
 
-        <div className="col-6 d-grid w-50 justify-content-center">
-          <FileUploader
-            multiple={true}
-            children={
-              <div className="upload-file">
-                <div className="d-flex align-items-center col-10">
-                  <i className="pi pi-file" style={{ fontSize: "22px" }} />
-                  <p className="ms-2 fs-5">
-                    {files.length > 0
-                     ? 'Precione para subir otro archivo'
-                      : "Subir o soltar un archivo aquí"}
-                  </p>
+        <div className="col-6 d-grid w-50 h-50 justify-content-start">
+          <div className="col-12">
+            <FileUploader
+              multiple={true}
+              children={
+                <div className="upload-file">
+                  <div className="d-flex align-items-center col-10">
+                    <i className="pi pi-file" style={{ fontSize: "22px" }} />
+                    <p className="ms-2 fs-5">
+                      {files.length > 0
+                        ? "Precione para subir otro archivo"
+                        : "Subir o soltar un archivo aquí"}
+                    </p>
+                  </div>
+                  <div className="d-flex justify-content-end col-2">
+                    <p className="fs-5">{fileTypes.join(", ")}</p>
+                  </div>
                 </div>
-                <div className="d-flex justify-content-end col-2">
-                  <p className="fs-5">{fileTypes.join(", ")}</p>
-                </div>
-              </div>
-            }
-            handleChange={handleChange}
-            name="file"
-            types={fileTypes}
-          />
-          <div className={`col-12 mt-4 ${files.length < 5 ? 'd-flex' : 'row' }`}>
+              }
+              handleChange={handleChange}
+              name="file"
+              types={fileTypes}
+            />
+          </div>
+          <div
+            className={`col-12 align-items-start mt-4 ${
+              files.length < 5 ? "d-flex" : "row"
+            }`}
+          >
             {files.length > 0
-             ? files.map((file, index) => (
-                  <div key={index} className="col-2 card w-card d-flex align-items-center justify-content-center me-4">
+              ? files.map((file, index) => (
+                  <div
+                    key={index}
+                    className="col-2 card w-card d-flex align-items-center justify-content-center me-4"
+                  >
                     <div className="file-item d-grid justify-content-center">
                       <i className="pi pi-file text-center size-file-card" />
-                      <p className="ms-2 w-p-card fs-5 text-center">{file.name.length > 22 ? file.name.slice(0, 22) + '... pdf' : file.name}</p>
+                      <p className="ms-2 w-p-card fs-5 text-center">
+                        {file.name.length > 22
+                          ? file.name.slice(0, 22) + "... pdf"
+                          : file.name}
+                      </p>
                     </div>
                   </div>
                 ))
