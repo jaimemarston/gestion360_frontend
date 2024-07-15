e4f116f4eb63bfef35e9104becbbcdc005a9ab90
xiover <xioveraldana123@gmail.com>
2024-07-14 23:54:38 -0400

    delete the table code and add the table component

--- src/Components/Sistema/Menu/FileManager/modals/register-folder.jsx
+++ src/Components/Sistema/Menu/FileManager/modals/register-folder.jsx
@@ -15,6 +15,7 @@ import classNames from "classnames";
 import FolderIcon from "@mui/icons-material/Folder";
 import React, { useState, useEffect, useRef } from "react";
 import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
+import TablaUsuario from '../../TableUsers'
 
 const RegisterFolder = ({
   isDarkMode,
@@ -405,84 +406,3 @@ const RegisterFolder = ({
 };
 
 export { RegisterFolder };
-
-const TablaUsuario = ({
-  dt,
-  listProduct,
-  selectedUsers,
-  setSelectedProducts,
-  globalFilter,
-  header,
-  codigoBodyTemplate,
-  nombreBodyTemplate,
-  usuarioBodyTemplate,
-  statusBodyTemplate,
-  AmountOfUsersBodyTemplate,
-}) => {
-  return (
-    <DataTable
-      ref={dt}
-      value={listProduct}
-      selection={selectedUsers}
-      onSelectionChange={(e) => setSelectedProducts(e.value)}
-      dataKey="id"
-      paginator
-      rows={10}
-      rowsPerPageOptions={[5, 10, 25]}
-      className="datatable-responsive"
-      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-      currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords}"
-      globalFilter={globalFilter}
-      emptyMessage="No products found."
-      header={header}
-      responsiveLayout="scroll"
-    >
-      <Column selectionMode="multiple" headerStyle={{ width: "3rem" }}></Column>
-      <Column
-        field="codigo"
-        header="Codigo"
-        sortable
-        body={codigoBodyTemplate}
-        headerStyle={{ width: "14%", minWidth: "10rem" }}
-      ></Column>
-      <Column
-        field="nombre"
-        header="Nombre"
-        sortable
-        body={nombreBodyTemplate}
-        headerStyle={{ width: "44%", minWidth: "10rem" }}
-      ></Column>
-      <Column
-        field="email"
-        header="Usuario"
-        sortable
-        body={usuarioBodyTemplate}
-        headerStyle={{ width: "14%", minWidth: "10rem" }}
-      ></Column>
-      <Column
-        field="rol"
-        header="Rol"
-        sortable
-        headerStyle={{ width: "14%", minWidth: "10rem" }}
-      ></Column>
-      {statusBodyTemplate &&
-        <Column
-          field="inventoryStatus"
-          header="Status"
-          body={statusBodyTemplate}
-          sortable
-          headerStyle={{ width: "14%", minWidth: "10rem" }}
-        ></Column>
-      }
-      {AmountOfUsersBodyTemplate &&
-        <Column
-          field="inventoryStatus"
-          header="Cantidad de usuarios"
-          body={AmountOfUsersBodyTemplate}
-          sortable
-          headerStyle={{ width: "18%", minWidth: "14rem" }}
-        ></Column>
-      }
-    </DataTable>
-  );
-};
--- src/Components/Sistema/Menu/UserGroupRegistration/index.jsx
+++ src/Components/Sistema/Menu/UserGroupRegistration/index.jsx
@@ -4,86 +4,13 @@ import {
     addUsersToTheUserGroup
 } from "../../../../store/slices/fileManager/fileManagerSlice";
 import { Button } from "primereact/button";
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
-import { Dialog } from "primereact/dialog";
 import { fetchGet } from "../../../../api";
 import { InputText } from "primereact/inputtext";
-import { Toolbar } from "primereact/toolbar";
 import { useDispatch } from "react-redux";
 import { useToast } from "../../../../hooks/useToast";
 import classNames from "classnames";
 import React, { useState, useEffect, useRef } from "react";
-import GroupIcon from '@mui/icons-material/Group';
-
-const TablaUsuario = ({
-    dt,
-    listProduct,
-    selectedUsers,
-    setSelectedProducts,
-    globalFilter,
-    header,
-    codigoBodyTemplate,
-    nombreBodyTemplate,
-    usuarioBodyTemplate,
-    statusBodyTemplate,
-}) => {
-    return (
-        <DataTable
-            ref={dt}
-            value={listProduct}
-            selection={selectedUsers}
-            onSelectionChange={(e) => setSelectedProducts(e.value)}
-            dataKey="id"
-            paginator
-            rows={10}
-            rowsPerPageOptions={[5, 10, 25]}
-            className="datatable-responsive"
-            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-            currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords}"
-            globalFilter={globalFilter}
-            emptyMessage="No products found."
-            header={header}
-            responsiveLayout="scroll"
-        >
-            <Column selectionMode="multiple" headerStyle={{ width: "3rem" }}></Column>
-            <Column
-                field="codigo"
-                header="Codigo"
-                sortable
-                body={codigoBodyTemplate}
-                headerStyle={{ width: "14%", minWidth: "10rem" }}
-            ></Column>
-            <Column
-                field="nombre"
-                header="Nombre"
-                sortable
-                body={nombreBodyTemplate}
-                headerStyle={{ width: "44%", minWidth: "10rem" }}
-            ></Column>
-            <Column
-                field="email"
-                header="Usuario"
-                sortable
-                body={usuarioBodyTemplate}
-                headerStyle={{ width: "14%", minWidth: "10rem" }}
-            ></Column>
-            <Column
-                field="rol"
-                header="Rol"
-                sortable
-                headerStyle={{ width: "14%", minWidth: "10rem" }}
-            ></Column>
-            <Column
-                field="inventoryStatus"
-                header="Status"
-                body={statusBodyTemplate}
-                sortable
-                headerStyle={{ width: "14%", minWidth: "10rem" }}
-            ></Column>
-        </DataTable>
-    );
-};
+import TablaUsuario from "../TableUsers";
 
 export default function CreateGroupUsers() {
     let empty = {
